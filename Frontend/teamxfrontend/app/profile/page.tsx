d30165602020e3dbeb3a082477513a6883ed6b8f
AasthaAgrawa <aasthaagrawa@umass.edu>
2024-05-07 19:39:28 -0400

    added features to profile page

--- Frontend/teamxfrontend/app/profile/page.tsx
+++ Frontend/teamxfrontend/app/profile/page.tsx
@@ -1,7 +1,8 @@
 "use client"
-
+import React, { useEffect, useState } from 'react';
 import { useLogin } from '../contexts/LoginContext';
 import Header from '../header/header';
+import SearchBar from '../component/search';
 import { useRouter } from 'next/navigation';
 import { useClubs } from '../contexts/ClubContext';
 
@@ -12,11 +13,17 @@ export default function Profile() {
   const { localUser } = useLogin();
   const { getClub } = useClubs();
   const router = useRouter();
+  const [searchQuery, setSearchQuery] = useState<string>('');
 
   if(!localUser) {
     router.push("/login?redirect=/profile", {})
     return <div>Redirecting to login...</div>
   }
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setSearchQuery(value);
+  };
 
   //this line is temporary until we want to view other users' pages while not signed in as them
   //they will be allowed to see a limited subset of information as compared to their own profile view
@@ -25,9 +32,17 @@ export default function Profile() {
   return (
     <>
     <Header />
+      <h1>Profile
+        <div className="search-bar">
+            <input type="text" placeholder="Search"/>
+            <button>Default</button>
+            <button>A-Z</button>
+            <button>List view</button>
+        </div>
+      </h1>
     <div className ="flow-root"></div>
-    <div className="flex items-center gap-5 py-9 pr-20 pl-20 mt-2 mb-1 max-w-full bg-green-300 rounded-3xl w-[795px] max-md:flex-wrap max-md:px-5">
-    <div className="items-center space-y">
+    <div className="flex items-center gap-10 py-9 pr-40 pl-40 mt-2 mb-1 max-w-full bg-green-300 rounded-3xl w-[795px] max-md:flex-wrap max-md:px-20">
+    <div className="items-left space-y">
         <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
           <h1 className="text-6xl font-bold">
           {user.name ? user.name : "No Name"}
@@ -46,6 +61,16 @@ export default function Profile() {
       </div>
     </div>
     </div>
+
+
+
+
+    <div className="flex">
+        {/* Search bar container */}
+        <div className="sticky-search bg-gray-100 p-4 top-0 z-10 h-screen w-64 border-r border-gray-300">
+          <SearchBar value={searchQuery} onChange={handleSearch} />
+        </div>
+    </div> 
     </>
   )
 }
