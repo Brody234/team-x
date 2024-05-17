dadd12ecdcfb18c7f99a309fdcd541bff2f04c1f
Saksham Bansal <sakshambansa@umass.edu>
2024-05-16 20:51:48 -0400

    Added comments to the event.js page

--- Backend/models/event.js
+++ Backend/models/event.js
@@ -1,47 +1,49 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
+// Define schema for the "event" collection
 const eventSchema = new Schema({
-    name:{
-        type: String,
-        required: true,
-        default: "Unnamed Club"
+    name: {
+        type: String, // Data type of 'name' is string
+        required: true, // 'name' is a mandatory field
+        default: "Unnamed Club" // Default value if 'name' is not specified
     },
     tags: {
-        type: [String],
-        require: true,
-        default: []
+        type: [String], // 'tags' is an array of strings
+        require: true, // 'tags' field is required
+        default: [] // Default value for 'tags' is an empty array
     },
     attendees: {
-        type: [Schema.Types.ObjectId],
-        require: true,
-        default: []
+        type: [Schema.Types.ObjectId], // 'attendees' is an array of ObjectIds
+        require: true, // 'attendees' field is required
+        default: [] // Default value for 'attendees' is an empty array
     },
     clubPosting: {
-        type: [Schema.Types.ObjectId],
-        require: true,
-        default: []
+        type: [Schema.Types.ObjectId], // 'clubPosting' is an array of ObjectIds, referring to clubs posting the event
+        require: true, // 'clubPosting' field is required
+        default: [] // Default value for 'clubPosting' is an empty array
     },
     startTime: {
-        type: Date,
-        require: true,
-        default: Date.now()
+        type: Date, // 'startTime' is a date field
+        require: true, // 'startTime' field is required
+        default: Date.now() // Default value for 'startTime' is the current date and time
     },
     location: {
-        type: String,
-        require: true,
-        default: "notanemail@gmail.com"
+        type: String, // Data type of 'location' is string
+        require: true, // 'location' field is required
+        default: "notanemail@gmail.com" // Default value for 'location' is a placeholder (incorrectly formatted as an email)
     },
     image: {
-        type: String,
-        require: true,
-        default: "notalink.com"
+        type: String, // Data type of 'image' is string
+        require: true, // 'image' field is required
+        default: "notalink.com" // Default value for 'image' is a placeholder (formatted as a link)
     },
     description: {
-        type: String,
-        require: true,
-        default: "This event is cool"
+        type: String, // Data type of 'description' is string
+        require: true, // 'description' field is required
+        default: "This event is cool" // Default description text
     }
-})
+});
 
+// Export the 'event' model with the defined schema
 module.exports = mongoose.model('event', eventSchema);
