bae0bb4b9e781fab2f496c688b3b6fcd12c4b9f6
Saksham Bansal <sakshambansa@umass.edu>
2024-05-16 20:52:46 -0400

    Added comments to the login.js page

--- Backend/models/login.js
+++ Backend/models/login.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
+// Define schema for the "Login" collection
 const loginSchema = new Schema({
     email: {
-        type: String,
-        required: true
+        type: String, // Data type of 'email' is string
+        required: true // 'email' is a required field, must be provided for each entry
     },
     saltedPasswordHash: {
-        type: String,
-        required: true,
+        type: String, // Data type of 'saltedPasswordHash' is string
+        required: true, // 'saltedPasswordHash' is a required field, stores the hashed password with salt
     }
-})
+});
 
+// Export the 'Login' model with the defined schema
 module.exports = mongoose.model('Login', loginSchema);
