8fb244bd83b273edc5f9b6a891ad79f08617142f
Saksham Bansal <sakshambansa@umass.edu>
2024-05-16 20:50:42 -0400

    Added comments to the user.js page

--- Backend/models/club.js
+++ Backend/models/club.js
@@ -1,36 +1,38 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
+// Define schema for the "club" collection
 const clubSchema = new Schema({
-    name:{
-        type: String,
-        required: true,
-        default: "Unnamed Club"
+    name: {
+        type: String, // Data type of 'name' is string
+        required: true, // 'name' is a required field
+        default: "Unnamed Club" // Default value if 'name' is not specified
     },
     tags: {
-        type: [Schema.Types.ObjectId],
-        require: true,
-        default: []
+        type: [Schema.Types.ObjectId], // 'tags' is an array of ObjectIds
+        require: true, // 'tags' field is required
+        default: [] // Default value for 'tags' is an empty array
     },
     events: {
-        type: [Schema.Types.ObjectId],
-        require: true,
-        default: []
+        type: [Schema.Types.ObjectId], // 'events' is an array of ObjectIds
+        require: true, // 'events' field is required
+        default: [] // Default value for 'events' is an empty array
     },
     owner: {
-        type: Schema.Types.ObjectId,
-        require: true
+        type: Schema.Types.ObjectId, // 'owner' is an ObjectId
+        require: true // 'owner' field is required
     },
     members: {
-        type: [Schema.Types.ObjectId],
-        require: true,
-        default: []
+        type: [Schema.Types.ObjectId], // 'members' is an array of ObjectIds
+        require: true, // 'members' field is required
+        default: [] // Default value for 'members' is an empty array
     },
     admins: {
-        type: [Schema.Types.ObjectId],
-        require: true,
-        default: []
+        type: [Schema.Types.ObjectId], // 'admins' is an array of ObjectIds
+        require: true, // 'admins' field is required
+        default: [] // Default value for 'admins' is an empty array
     },
-})
+});
 
+// Export the 'club' model with the defined schema
 module.exports = mongoose.model('club', clubSchema);
